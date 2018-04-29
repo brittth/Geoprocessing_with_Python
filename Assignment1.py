f9dfc17f5fbb7822f06fc7730cedea66a3966574
Britta <britta.themann@web.de>
2018-04-29 23:52:25 +0200

    raw script from Matthias

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,25 @@
-h= "Hello"
+# ####################################### LOAD REQUIRED LIBRARIES ############################################# #
+import time
+import ogr
+import baumiTools as bt
+# ####################################### SET TIME-COUNT ###################################################### #
+starttime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("--------------------------------------------------------")
+print("Starting process, time: " + starttime)
+print("")
+# ####################################### FOLDER PATHS & global variables ##################################### #
+SHP = "L:/_SHARED_DATA/CL_MB/tc_sc/_Version02_300m/points_300m_clip.shp"
+outputFile = "L:/_SHARED_DATA/CL_MB/tc_sc/_Version02_300m/points_300m_clip_summary.shp"
+buff_m = 100
+# ####################################### PROCESSING ########################################################## #
+
+# DO YOUR PROCESSING CODE HERE
+
+# ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
+print("")
+endtime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("start: " + starttime)
+print("end: " + endtime)
+print("")
