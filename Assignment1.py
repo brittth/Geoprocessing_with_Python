14606ef853f2e10d1396262f8c7527442e157e0a
Britta <britta.themann@web.de>
2018-05-01 12:59:28 +0200

    Assignment 1 - Excersize I/1) finished

--- Assignment1.py
+++ Assignment1.py
@@ -5,7 +5,8 @@ import ogr
 import baumiTools as bt
 '''
 import os
-import fnmatch
+import re
+
 # ####################################### SET TIME-COUNT ###################################################### #
 '''
 starttime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
@@ -21,6 +22,7 @@ buff_m = 100
 '''
 footprints = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 2 - IDE, debugger, first scripts/Assignment01_data/Part01_Landsat/"
 # ####################################### PROCESSING ########################################################## #
+# EXERCISE I - 1)
 #create empty lists for names of and paths to each of the footprint folders
 foldername_fp_list =[]
 dir_list_fp = []
@@ -30,60 +32,26 @@ dir_list_fp = []
 for foldername_fp in os.listdir(footprints):#for current directory, use: ('.')
     foldername_fp_list.append(foldername_fp)
     dir_list_fp.append(footprints + foldername_fp)
-print(foldername_fp_list)
-print(dir_list_fp) #list of footprint paths (path_fp)
-
-#create empty list for lists of scenes within each footprint
-fp_sc_list = []
-
-#extract a file list from each footprint folder and write it into the empty list of lists
-for path_fp in dir_list_fp:
-    for foldername_fp in foldername_fp_list:
-        foldername_fp =[]
-        foldername_fp.append(os.listdir(path_fp))
-    fp_sc_list.append(foldername_fp[0]) #the 0 is only to get rid of double brackets
-print(fp_sc_list)
-print(len(fp_sc_list))
-
-#count scenes per footprint
-for i in range(len(fp_sc_list)):
-    #if fnmatch.fnmatch(i, 'LC08*'):
-        no= len(fp_sc_list[i])
-        print(no)
+#print(foldername_fp_list)
+#print(dir_list_fp) #list of footprint paths (path_fp)
 
 #count scenes per sensor for each footprint
-#for i in range(len(fp_sc_list)):
-#    noLC08 = {}
-#    noLE07 = {}
-#    noLT05 = {}
+for path_fp in dir_list_fp:
+    #print(os.listdir(path_fp))
+    ls5 = re.compile("^LT05")
+    ls7 = re.compile("^LE07")
+    ls8 = re.compile("^LC08")
+    L5 = filter(ls5.match, os.listdir(path_fp))
+    L7 = filter(ls7.match, os.listdir(path_fp))
+    L8 = filter(ls8.match, os.listdir(path_fp))
+    print("For footprint " + str(path_fp[-7:]) + ", there are " + str(len(list(L5))) + " Landsat 5, " + str(len(list(L7))) + " Landsat 7 and " + str(len(list(L8))) + " Landsat 8 scenes.")
+
+
+# EXERCISE I - 2)
 
 
 
 
-
-''' #DYSFUNCIONAL
-# find number of subdirectories in a directory, folders only
-
-for x in os.listdir(ass1_footprints):
-    if os.path.isdir(x):
-        ass1_dir_list = [ass1_footprints + x]
-    else: continue
-    print(ass1_dir_list)
-'''
-
-
-'''
-ass1_footprints_LC08 = []
-for row in ds_v1:
-    print(row.index)
-    row = row.replace("\n","")
-    cleanList.append(row)
-print(cleanList)
-
-for name in correctedNames:
-    if name.count(" ") > 2:
-        print(name)
-'''
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 '''
 print("")
@@ -94,3 +62,54 @@ print("start: " + starttime)
 print("end: " + endtime)
 print("")
 '''
+
+# ####################################### TEMPLATES ########################################################## #
+''' #DYSFUNCIONAL
+# find number of subdirectories in a directory, folders only
+for x in os.listdir(ass1_footprints):
+    if os.path.isdir(x):
+        ass1_dir_list = [ass1_footprints + x]
+    else: continue
+    print(ass1_dir_list)
+'''
+
+'''
+#create empty list for lists of scenes within each footprint
+fp_sc_list = []
+
+#extract a file list from each footprint folder and write it into the empty list
+for path_fp in dir_list_fp:
+    for foldername_fp in foldername_fp_list:
+        foldername_fp =[]
+        foldername_fp.append(os.listdir(path_fp))
+    fp_sc_list.append(foldername_fp[0]) #the 0 is only to get rid of double brackets
+print(fp_sc_list)
+print(len(fp_sc_list))
+'''
+
+'''
+#working template for regular expressions
+mylist = ["dog", "cat", "wildcat", "thundercat", "cow", "hooo"]
+r = re.compile(".*cat")
+newlist = filter(r.match, mylist)
+print (list(newlist))
+'''
+
+'''
+mylist=fp_sc_list[0]
+print (mylist)
+
+# only works for one letter
+def countStringStart(mylist):
+    count = 0
+    for item in mylist:
+        if item[0].startswith('L'):
+            count +=1
+    return count
+print(countStringStart(mylist))
+'''
+
+#count scenes per footprint
+#for i in range(len(fp_sc_list)):
+#        no= len(fp_sc_list[i])
+#        print(no)
