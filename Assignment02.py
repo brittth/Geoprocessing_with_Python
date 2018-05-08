db891c5191044f83689a7bcd729ca552ae3162fd
Britta <britta.themann@web.de>
2018-05-08 12:37:24 +0200

    Assignment02 - complete

--- Assignment02.py
+++ Assignment02.py
@@ -14,27 +14,73 @@ print("")
 
 # ####################################### FOLDER PATHS & global variables ##################################### #
 
-root_folder = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 4 - Raster processing I/"
+root_folder = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 4 - Raster processing I/Assignment02_data/"
 
 # ####################################### PROCESSING ########################################################## #
 
-ds = gdal.Open(root_folder + "LT05_L1TP_117056_20000717_20161214_01_T1_sr_evi.tif", gdal.GA_ReadOnly)
+file_list = os.listdir(root_folder)
+#print(file_list)
 
-gt = ds.GetGeoTransform()
-#pr = ds.GetProjection()
-#cols = ds.RasterXSize
-#rows = ds.RasterYSize
-#print(cols,rows)
+#(a) calculate the corner coordinates for each of the raster files
+print("\nAssignment02 a)\n")
 
-print(gt)
-UL_x, UL_y = gt[0], gt[3]
-# Upper left
-print(UL_x, UL_y)
-#Lower right
-LR_x = UL_x + (gt[1]*ds.RasterXSize)
-LR_y = UL_y + (gt[5]*ds.RasterYSize)
-print(LR_x)
-print(LR_y)
+#GetCoordinates --> get coordinates for multiple tif files
+    # input: list of input files --> file_list
+    # UL --> Upper Left corner
+    # LR --> Lower Right corner
+def GetCoordinates(file_list):
+    for file in file_list:
+        ds = gdal.Open(root_folder + file, gdal.GA_ReadOnly)
+        gt = ds.GetGeoTransform()  # UL_x, x-coordinate spatial resolution, UL_y, # y-coord. spat.res.
+        # Upper left
+        UL_x, UL_y = gt[0], gt[3]
+        # Lower right
+        LR_x = UL_x + (gt[1] * ds.RasterXSize)
+        LR_y = UL_y + (gt[5] * ds.RasterYSize)
+        print("Coordinates of " + file + ": " + "(" + str(UL_x) + "," + str(UL_y) + ") and (" + str(LR_x) + "," + str(LR_y) + ")")
+
+GetCoordinates(file_list)
+
+
+
+#(b) find the overlapping area across all raster files
+print("\nAssignment02 b)\n")
+
+#GetIntersectCoordinates --> get coordinates of the largest common extent of multiple tif files
+    # input: list of input files --> file_list
+    # UL --> Upper Left corner
+    # LR --> Lower Right corner
+def GetIntersectCoordinates(file_list):
+    UL_x_list = []
+    UL_y_list = []
+    LR_x_list = []
+    LR_y_list = []
+    for file in file_list:
+        ds = gdal.Open(root_folder + file, gdal.GA_ReadOnly)
+        gt = ds.GetGeoTransform()  # UL_x, x-coordinate spatial resolution, UL_y, # y-coord. spat.res.
+        # Upper left
+        UL_x, UL_y = gt[0], gt[3]
+        UL_x_list.append(UL_x)
+        UL_y_list.append(UL_y)
+        # Lower right
+        LR_x = UL_x + (gt[1] * ds.RasterXSize)
+        LR_y = UL_y + (gt[5] * ds.RasterYSize)
+        LR_x_list.append(LR_x)
+        LR_y_list.append(LR_y)
+    UL_x_ext = max(UL_x_list)
+    UL_y_ext = min(UL_y_list)
+    LR_x_ext = min(LR_x_list)
+    LR_y_ext = max(LR_y_list)
+    LL_x_ext = UL_x_ext
+    LL_y_ext = LR_y_ext
+    UR_x_ext = LR_x_ext
+    UR_y_ext = UL_y_ext
+    print("The coordinates of the largest common extent are as follows:\n Upper left corner: (" + str(UL_x_ext) + "," + str(
+        UL_y_ext) + ")\n Upper right corner: (" + str(UR_x_ext) + "," + str(UR_y_ext) + ")\n Lower left corner: (" + str(
+        LL_x_ext) + "," + str(LL_y_ext) + ")\n Lower right corner: (" + str(LR_x_ext) + "," + str(LR_y_ext) + ")")
+
+GetIntersectCoordinates(file_list)
+
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 
 print("")
