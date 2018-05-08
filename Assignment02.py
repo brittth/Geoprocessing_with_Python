ea4072411fe871b23b2150883bfa83aec762d435
Britta <britta.themann@web.de>
2018-05-08 10:37:47 +0200

    Assignment02 - started

--- Assignment02.py
+++ Assignment02.py
@@ -1,6 +1,7 @@
 # ####################################### LOAD REQUIRED LIBRARIES ############################################# #
 
 import time
+import gdal
 import os
 import re
 
@@ -13,12 +14,27 @@ print("")
 
 # ####################################### FOLDER PATHS & global variables ##################################### #
 
-
+root_folder = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 4 - Raster processing I/"
 
 # ####################################### PROCESSING ########################################################## #
 
+ds = gdal.Open(root_folder + "LT05_L1TP_117056_20000717_20161214_01_T1_sr_evi.tif", gdal.GA_ReadOnly)
 
+gt = ds.GetGeoTransform()
+#pr = ds.GetProjection()
+#cols = ds.RasterXSize
+#rows = ds.RasterYSize
+#print(cols,rows)
 
+print(gt)
+UL_x, UL_y = gt[0], gt[3]
+# Upper left
+print(UL_x, UL_y)
+#Lower right
+LR_x = UL_x + (gt[1]*ds.RasterXSize)
+LR_y = UL_y + (gt[5]*ds.RasterYSize)
+print(LR_x)
+print(LR_y)
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 
 print("")
