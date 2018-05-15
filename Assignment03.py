2eef486226789015596d1f4937705ce2dbd948f6
Britta <britta.themann@web.de>
2018-05-15 11:51:40 +0200

    Assignment03 - Partially copied from board

--- Assignment03.py
+++ Assignment03.py
@@ -2,7 +2,7 @@
 
 import time
 import os
-import re
+import gdal
 
 # ####################################### SET TIME-COUNT ###################################################### #
 
@@ -12,11 +12,24 @@ print("Starting process, time: " + starttime)
 print("")
 
 # ####################################### FOLDER PATHS & global variables ##################################### #
-
+root_folder = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 5 - Raster processing II/"
 
 
 # ####################################### PROCESSING ########################################################## #
+# Open Raster
+ds = gdal.open(root_folder + file)
 
+# Get the basic properties of the raster file
+gt = ds.GetGeoTransform()
+pr = ds.GetProjection()
+cols = ds.RasterXSize
+rows = ds.RasterYSize
+nbands = ds.RasterCount
+
+# Get the raster values (from the entire raster)
+rb = ds.GetRasterBand(1)
+dtype = rb.DataType
+arr = rb.ReadAsArray(0,0,cols,rows) # (origin_x, origin_y, sliceSize_x, SliceSize_y)
 
 
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
