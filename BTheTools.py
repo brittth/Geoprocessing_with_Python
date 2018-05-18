2ef8d69c1e2ee9c449c55aa74558f3b86fd15ef5
Britta <britta.themann@web.de>
2018-05-18 16:42:48 +0200

    BTheTools - Assignment03 I 2) ThresholdBinaryArrayMask added

--- BTheTools.py
+++ BTheTools.py
@@ -219,6 +219,38 @@ def RasterOverlapToArray(file_list):
         # UL x offset:  109
         # UL y offset:  177
 
+#ThresholdBinaryArrayMask --> create a binary mask based on a threshold, 1s statement fulfilled, 0s all others
+    #input: array, which will be overwritte, so copy if original should be kept
+    #       operator --> type as string
+    #       threshold --> type as number
+def ThresholdBinaryArrayMask(array, operator, threshold):
+    if operator == '<':
+        array[array < threshold] = 1  # replace all values <1000 with 1
+    elif operator == '>':
+        array[array > threshold] = 1  # replace all values <1000 with 1
+    elif operator == '<=':
+        array[array <= threshold] = 1  # replace all values <1000 with 1
+    elif operator == '>=':
+        array[array >= threshold] = 1  # replace all values <1000 with 1
+    elif operator == '==':
+        array[array == threshold] = 1  # replace all values <1000 with 1
+    elif operator == '!=':
+        array[array != threshold] = 1  # replace all values <1000 with 1
+    array[array != 1] = 0  # replace all values other than 1 with 0
+    #returns: overwrites input array with binary mask
+    #application example:
+    # arr_dem_mask = arr_dem #to be overwritten
+    # ThresholdBinaryArrayMask(arr_dem_mask, "<", 1000)
+    # print(arr_mask)
+        #returns:
+        # [[0.0 0.0 0.0 ... 0.0 0.0 0.0]
+        # [0.0 0.0 0.0 ... 0.0 0.0 0.0]
+        # [0.0 0.0 0.0 ... 0.0 0.0 1.0]
+        # ...
+        # [1.0 1.0 1.0 ... 0.0 0.0 0.0]
+        # [1.0 1.0 1.0 ... 0.0 0.0 0.0]
+        # [1.0 1.0 1.0 ... 0.0 0.0 0.0]]
+
 # ####################################### TEMPLATES ########################################################## #
 
 #count scenes per sensor for each folder (ex. footprint)
