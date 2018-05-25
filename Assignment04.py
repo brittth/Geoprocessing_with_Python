b09223f4dae90eb7afe97b92d98a99c092213f9d
Britta <britta.themann@web.de>
2018-05-25 16:59:48 +0200

    Assignment04 - (Friday night Jule&Britta) funktioniert soweit,mit for-loop anstatt while loop/GetNextFeature, nicht komplett

--- Assignment04.py
+++ Assignment04.py
@@ -47,30 +47,60 @@ wd = 'D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 6 - Vec
 #Open a shapefile
 countries = ogr.Open(wd+"gadm36_dissolve.shp")
 protected = ogr.Open(wd+"WDPA_May2018-shapefile-polygons.shp")
+ctest = ogr.Open(wd+"ctest.shp")
+
 #Opening a layer
 layer_countries = countries.GetLayer()
 layer_protected = protected.GetLayer()
+layer_ctest = ctest.GetLayer()
 
 #OUR LOOP
-feature_countries = layer_countries.GetNextFeature()
-while feature_countries:
-    # get info from country layer
-    name = feature_countries.GetField('NAME_0')
-    layer_countries.SetAttributeFilter("NAME_0 ='"+name+"'")
-    feature_countries = layer_countries.GetNextFeature()
-    country = feature_countries.geometry().Clone()
-    layer_protected.SetSpatialFilter(country)
-    feature_count = layer_protected.GetFeatureCount()           #works till here
-    layer_protected.SetSpatialFilter(None)                      #no error, but doesn't do anything either
-        # Probably in the next code line is the problem,
-        # since the filter on the layer_countries is still active
-        # which results in the layer containing only one feature.
-        # I tried already to work with a second layer_countries,
-        # but somewhoe this didn't work.
-        # And it doesn't seem like a good solution.
-    feature_countries = layer_countries.GetNextFeature()
-    print('Name:', name, '#Protected Areas:', feature_count)
-layer_countries.ResetReading() #necessary if looping again
+num_countries = layer_ctest.GetFeatureCount()
+nums = list(range(0,num_countries))
+#print(nums)
+
+clist = []
+for index in nums:
+    #Get a specfic Feature via Index
+    clist.append(layer_countries.GetFeature(index))
+#print(clist)
+
+for country in clist:
+    #Country Spatial Filter
+    id = country.GetField('ID_0')
+    name = country.GetField('NAME_0')
+    layer_countries.SetAttributeFilter("NAME_0 ='" + name + "'")
+    countryshp = country.geometry().Clone()
+    layer_protected.SetSpatialFilter(countryshp)
+    #Protected Basic Attribute Filter
+    layer_protected.SetAttributeFilter("STATUS = 'Designated'")#   STATUS == Established OR Designated --> Established muss noch dazu, hat mit OR nicht funktioniert
+    layer_protected.SetAttributeFilter("MARINE = '0'")#   MARINE string [0 (terrestrial), 1 (coastal), 2 (marine)];
+    feature_count = layer_protected.GetFeatureCount()
+    #Protected Category Filters
+    # Ia
+    layer_protected.SetAttributeFilter("IUCN_CAT = 'Ia'")# IUCN_CAT    string  [1-VI, not reported, not applicable] --> Andere Kategorien fehlen noch
+    feature_countIa=layer_protected.GetFeatureCount()
+    layer_protected.SetAttributeFilter(None)
+    # Ib
+    layer_protected.SetAttributeFilter("STATUS = 'Designated'")# Wiederholt von oben
+    layer_protected.SetAttributeFilter("MARINE = '0'")
+    layer_protected.SetAttributeFilter("IUCN_CAT = 'Ib'")
+    feature_countIb = layer_protected.GetFeatureCount()
+    layer_protected.SetAttributeFilter(None)
+    # HIER KOMMEN ALLE ANDEREN FILTER NOCH NACH DEM GLEICHEN PRINZIP
+    layer_protected.SetSpatialFilter(None)
+    print('Country ID: ',id,'\n',
+          'Country Name:',name,'\n',
+          'PA Category: noch nichts','\n',
+          '# PAs:', feature_count,'\n',
+          'PA Category Ia: ', feature_countIa,'\n',
+          'PA Category Ib: ', feature_countIb,'\n',
+          'Mean area of PAs: noch nichts','\n',
+          'Area of largest PA: noch nichts','\n',
+          'Name of largest PA: noch nichts','\n',
+          'Year of etsabl. of largest PA: noch nichs\n\n')
+
+
 
 
 # #### END TIME-COUNT AND PRINT TIME STATS #### #
