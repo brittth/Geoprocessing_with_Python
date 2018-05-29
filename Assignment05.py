8b72cca4e70d6bb4f50f4d41dacb383191b813ab
Britta <britta.themann@web.de>
2018-05-29 12:08:53 +0200

    Assignment05 - Group I concept

--- Assignment05.py
+++ Assignment05.py
@@ -2,10 +2,11 @@
     #1) Get one PA
     #2) transform to UTM
     #3) Get extent of PA
-    #4) Random Point within extent based on starting point
+    #4) Random Point within extent based on starting point (multitude of 30m)
     #5) Check if point within borders of PA (not extent)
     #6) Check if point has min x meters distance to nearest border
     #7) Check if x-coord. >= 90m
     #       else check if |y-coord| >= 90m
     #   --> both false, start again
-    #   --> one TRUE save list
+    #   --> one TRUE save list1
+    # while loop stopping at 50 entries
