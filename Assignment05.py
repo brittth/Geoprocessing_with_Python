441393ab50246e0f38e4f4138b4d8a32b6e65f5c
Britta <britta.themann@web.de>
2018-05-29 12:36:28 +0200

    Assignment05 - Group 5/6 concept

--- Assignment05.py
+++ Assignment05.py
@@ -1,6 +1,6 @@
 #Group I: Random Sampling
     #1) Get one PA
-    #2) transform to UTM
+    #2) transform to EPSG 3035
     #3) Get extent of PA
     #4) Random Point within extent based on starting point (multitude of 30m)
     #5) Check if point within borders of PA (not extent)
@@ -10,3 +10,10 @@
     #   --> both false, start again
     #   --> one TRUE save list1
     # while loop stopping at 50 entries
+
+#Group II: Building the pixels
+    # 9 Boxes by adding ad subtracting from given center points
+    # add ID field for each 9 pixels and for each 9-pixel-box
+
+#Group III: Coordinate system and shapefiles
+    # EPSG 3035 because it is in meters
