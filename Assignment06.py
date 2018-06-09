b61f68a9a228bc1a8b9fa5456063a5ae15234ce5
Britta <britta.themann@web.de>
2018-06-09 02:56:52 +0200

    Assignment06 - try for all windows on all files, unsure if works

--- Assignment06.py
+++ Assignment06.py
@@ -36,7 +36,7 @@ def make_slices(data, rows, cols):
             # counter += 1
             data_st = data[i:rows+i,j:cols+j]
             arr1d = data_st.flatten()
-        slices.append(arr1d) #indent one more to the right for 980100 --> AS IS, SLICES ONLY FOR FIRST ROW
+            slices.append(arr1d) #indent one more to the right for 980100 --> AS IS, SLICES ONLY FOR FIRST ROW
     print('\nWindow size: (',rows,',',cols,')')
     print('Number of windows: ',counter)
     sl_arr = np.asarray(slices)
@@ -74,28 +74,27 @@ def chunks(l, n):
         # Create an index range for l of n items:
         yield l[i:i+n]
 
-'''
-def make_raster(in_ds, fn, data, data_type, nodata=None):
-    """Create a one-band GeoTIFF.
-    in_ds - datasource to copy projection and geotransform from
-    fn - path to the file to create
-    data - NumPy array containing data to write
-    data_type - output data type
-    nodata - optional NoData value
-    """
-    driver = gdal.GetDriverByName('GTiff')
-    out_ds = driver.Create(
-    fn, in_ds.RasterXSize, in_ds.RasterYSize, 1, data_type)
-    out_ds.SetProjection(in_ds.GetProjection())
-    out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-    out_band = out_ds.GetRasterBand(1)
-    if nodata is not None:
-        out_band.SetNoDataValue(nodata)
-        out_band.WriteArray(data)
-        out_band.FlushCache()
-        out_band.ComputeStatistics(False)
-        return out_ds
-'''
+def insertArray(insertion_array, window_size):
+    arr = np.zeros(1000*1000).reshape(1000,1000)
+    arr[int((window_size-1)/2):int(((window_size-1)/2)+(1000-window_size+1)), int((window_size-1)/2):int(((window_size-1)/2)+(1000-window_size+1))] = insertion_array
+    return arr
+
+def Array2Raster(outfile_name, originfile_path, x_offset, y_offset, array, outfile_datatype):
+    ds = gdal.Open(originfile_path)
+    # Get the basic properties of the raster file
+    gt = ds.GetGeoTransform()
+    pr = ds.GetProjection()
+    cols = ds.RasterXSize
+    rows = ds.RasterYSize
+    nbands = ds.RasterCount
+    # 1. Create a driver with which we write the output
+    drvR = gdal.GetDriverByName('GTiff')
+    # 2. Create the file (here: although exactly the same, we go through the syntax)
+    outDS = drvR.Create(outfile_name, cols, rows, nbands, outfile_datatype)
+    outDS.SetProjection(pr)
+    outDS.SetGeoTransform(gt)
+    # 3. Write the array into the newly generated file
+    outDS.GetRasterBand(1).WriteArray(array, x_offset, y_offset) # (array, offset_x, offset_y)
 
 # ####################################### PROCESSING ########################################################## #
 # Moving window sizes:(a) 150m, (b) 300m, and (c) 450m
@@ -104,148 +103,135 @@ def make_raster(in_ds, fn, data, data_type, nodata=None):
 # 2, 3, 5, 11, 13, 18, 19 --> Open habitat in 2015 (pasture, natural grassland, savanna)
 
 #load raster as array
-#ds = gdal.Open(tile1)
-#band1 = ds.GetRasterBand(1)
-#t1 = np.array(ds.ReadAsArray())
-#ds = gdal.Open(tile2)
-#t2 = np.array(ds.ReadAsArray())
+ds = gdal.Open(tile1)
+t1 = np.array(ds.ReadAsArray())
+ds = gdal.Open(tile2)
+t2 = np.array(ds.ReadAsArray())
 ds = gdal.Open(tile3)
 t3 = np.array(ds.ReadAsArray())
 
 #calculate window size in pixels
 w150 = int((150/30*2)+1)
-#w300 = int((300/30*2)+1)
-#w450 = int((450/30*2)+1)
+w300 = int((300/30*2)+1)
+w450 = int((450/30*2)+1)
 
 #make window slices --> make_slices(data, x_win_size, y_win_size)
-#sl1_150 = make_slices(t1,w150,w150)
-#sl1_300 = make_slices(t1,w300,w300)
-#sl1_450 = make_slices(t1,w450,w450)
+sl1_150 = make_slices(t1,w150,w150)
+sl1_300 = make_slices(t1,w300,w300)
+sl1_450 = make_slices(t1,w450,w450)
 
-#sl2_150 = make_slices(t2,w150,w150)
-#sl2_300 = make_slices(t2,w300,w300)
-#sl2_450 = make_slices(t2,w450,w450)
+sl2_150 = make_slices(t2,w150,w150)
+sl2_300 = make_slices(t2,w300,w300)
+sl2_450 = make_slices(t2,w450,w450)
 
 sl3_150 = make_slices(t3,w150,w150)
-#sl3_300 = make_slices(t3,w300,w300)
-#sl3_450 = make_slices(t3,w450,w450)
+sl3_300 = make_slices(t3,w300,w300)
+sl3_450 = make_slices(t3,w450,w450)
 
 # calculate shdi for each slice
 cat_list = [1, 17, 2, 3, 5, 11, 13, 18, 19]
-#np.apply_along_axis(calculateSHDI(cat_list, sl3_150), 1, sl3_150) #--> ERROR
-counter = 0
-shdi_list = []
-for i in sl3_150: #if loop directly in function above, then error
-    counter += 1
-    print('\nSlice #',counter,':')
-    shdi = calculateSHDI(cat_list,sl3_150[i])
-    shdi_list.append(shdi) #save shdi values in list
-print(len(shdi_list))
+data_list = [sl1_150,sl1_300,sl1_450,sl2_150,sl2_300,sl2_450,sl3_150,sl3_300,sl3_450]
+shdi_list_list = [0] * 9
+iter = 0
+for element in data_list:
+    counter = 0
+    shdi_list =[]
+    for i in element: #if loop directly in function above, then error
+        counter += 1
+        print('\nSlice #',counter,':')
+        shdi = calculateSHDI(cat_list,element[i])
+        shdi_list.append(shdi) #save shdi values in list
+    shdi_list_list[iter] = shdi_list
+    iter += 1
+#print(shdi_list_list)
 
-# divide shdi_list into chucks of 990 length
-shdi_chunks = list(chunks(shdi_list, 990)) #list of 1 list with 990 values, should be more when more than 1 row is considered
-print(shdi_chunks)
-print(len(shdi_chunks))
+shdi_list_sl1_150 = shdi_list_list[0]
+shdi_list_sl1_300 = shdi_list_list[1]
+shdi_list_sl1_450 = shdi_list_list[2]
+
+shdi_list_sl2_150 = shdi_list_list[3]
+shdi_list_sl2_300 = shdi_list_list[4]
+shdi_list_sl2_450 = shdi_list_list[5]
+
+shdi_list_sl3_150 = shdi_list_list[6]
+shdi_list_sl3_300 = shdi_list_list[7]
+shdi_list_sl3_450 = shdi_list_list[8]
+
+shdi_list_list.clear() #delete list
+print('SHDI List created')
+
+# divide shdi_list into chucks of certain length
+shdi_chunks_sl1_150 = list(chunks(shdi_list, (1000-w150+1))) #list of 990 lists with 990 values
+shdi_chunks_sl2_150 = list(chunks(shdi_list, (1000-w150+1)))
+shdi_chunks_sl3_150 = list(chunks(shdi_list, (1000-w150+1)))
+print('SHDI Lists in chunks created with chunk length of ', len(shdi_chunks_sl3_150),'.')
+
+shdi_chunks_sl1_300 = list(chunks(shdi_list, (1000-w300+1))) #list of 980 lists with 980 values
+shdi_chunks_sl2_300 = list(chunks(shdi_list, (1000-w300+1)))
+shdi_chunks_sl3_300 = list(chunks(shdi_list, (1000-w300+1)))
+print('SHDI Lists in chunks created with chunk length of ', len(shdi_chunks_sl3_300),'.')
+
+shdi_chunks_sl1_450 = list(chunks(shdi_list, (1000-w450+1))) #list of 970 lists with 970 values
+shdi_chunks_sl2_450 = list(chunks(shdi_list, (1000-w450+1)))
+shdi_chunks_sl3_450 = list(chunks(shdi_list, (1000-w450+1)))
+print('SHDI Lists in chunks created with chunk length of ', len(shdi_chunks_sl3_450),'.')
 
 #convert shdi_chunks into an array
-shdi_arr = np.asarray(shdi_chunks)
-print(shdi_arr.shape)
-print(shdi_arr.dtype)
+shdi_arr_sl1_150 = np.asarray(shdi_chunks_sl1_150)
+shdi_arr_sl2_150 = np.asarray(shdi_chunks_sl2_150)
+shdi_arr_sl3_150 = np.asarray(shdi_chunks_sl3_150)
+print('The SHDI arrays have a shape of:',shdi_arr_sl3_150.shape)
+print('The SHDI arrays have the data type: ',shdi_arr_sl3_150.dtype)
+
+shdi_arr_sl1_300 = np.asarray(shdi_chunks_sl1_300)
+shdi_arr_sl2_300 = np.asarray(shdi_chunks_sl2_300)
+shdi_arr_sl3_300 = np.asarray(shdi_chunks_sl3_300)
+print('The SHDI arrays have a shape of:',shdi_arr_sl3_300.shape)
+print('The SHDI arrays have the data type: ',shdi_arr_sl3_300.dtype)
+
+shdi_arr_sl1_450 = np.asarray(shdi_chunks_sl1_450)
+shdi_arr_sl2_450 = np.asarray(shdi_chunks_sl2_450)
+shdi_arr_sl3_450 = np.asarray(shdi_chunks_sl3_450)
+print('The SHDI arrays have a shape of:',shdi_arr_sl3_450.shape)
+print('The SHDI arrays have the data type: ',shdi_arr_sl3_450.dtype)
+
+#insert smaller array into a larger zeros array
+arr_sl1_150 = insertArray(shdi_arr_sl1_150, w150)
+arr_sl2_150 = insertArray(shdi_arr_sl2_150, w150)
+arr_sl3_150 = insertArray(shdi_arr_sl3_150, w150)
+print('The smaller arrays of size',shdi_arr_sl3_150.shape,'was inserted into the larger array of size',arr_sl3_150.shape,'.')
+
+arr_sl1_300 = insertArray(shdi_arr_sl1_300, w300)
+arr_sl2_300 = insertArray(shdi_arr_sl2_300, w300)
+arr_sl3_300 = insertArray(shdi_arr_sl3_300, w300)
+print('The smaller arrays of size',shdi_arr_sl3_300.shape,'was inserted into the larger array of size',arr_sl3_300.shape,'.')
+
+arr_sl1_450 = insertArray(shdi_arr_sl1_450, w450)
+arr_sl2_450 = insertArray(shdi_arr_sl2_450, w450)
+arr_sl3_450 = insertArray(shdi_arr_sl3_450, w450)
+print('The smaller arrays of size',shdi_arr_sl3_450.shape,'was inserted into the larger array of size',arr_sl3_450.shape,'.')
+
+    #smaller test array for demonstration --> works perfectly
+#arr3 = np.zeros(25).reshape(5,5)
+#print(arr3.shape)
+#arr4 = np.ones(9).reshape(3,3)
+#print(arr4.shape)
+#arr3[1:4,1:4] = arr4
+#print(arr3.shape)
+#print(arr3)
 
 #convert array to raster and write to disc
-#outfile = drvR.Create(outPath, cols, rows, 1, gdal.GDT_Float32) #robert(forum)
-'''
-# ATTEMPT 3 FROM STACKOVERFLOW WEBSITE
-arr = np.zeros(100).reshape(10,10)
-arr2 = np.ones(4).reshape(2,2)
-arr[4:6, 4:6] = arr2
-print(arr)
-'''
+Array2Raster('1_150.tif', tile1, 0, 0, arr_sl1_150, gdal.GDT_Float64)
+Array2Raster('1_300.tif', tile1, 0, 0, arr_sl1_300, gdal.GDT_Float64)
+Array2Raster('1_450.tif', tile1, 0, 0, arr_sl1_450, gdal.GDT_Float64)
 
-# ATTEMPT 2 FROM COOKBOOK WEBSITE
-def array2raster(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,array):
-    cols = array.shape[1]
-    rows = array.shape[0]
-    originX = rasterOrigin[0]
-    originY = rasterOrigin[1]
+Array2Raster('2_150.tif', tile2, 0, 0, arr_sl2_150, gdal.GDT_Float64)
+Array2Raster('2_300.tif', tile2, 0, 0, arr_sl2_300, gdal.GDT_Float64)
+Array2Raster('2_450.tif', tile2, 0, 0, arr_sl2_450, gdal.GDT_Float64)
 
-    driver = gdal.GetDriverByName('GTiff')
-    outRaster = driver.Create(newRasterfn, cols, rows, 1, gdal.GDT_Byte)
-    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
-    outband = outRaster.GetRasterBand(1)
-    outband.WriteArray(array)
-    outRasterSRS = osr.SpatialReference()
-    outRasterSRS.ImportFromEPSG(4326)
-    outRaster.SetProjection(outRasterSRS.ExportToWkt())
-    outband.FlushCache()
-result = array2raster(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,array)
-print(result)
-
-''' # ATTEMPT 1 FROM BOOK CHAPTER 
-"""Create a one-band GeoTIFF.
-in_ds - datasource to copy projection and geotransform from
-fn - path to the file to create
-data - NumPy array containing data to write
-data_type - output data type
-nodata - optional NoData value
-"""
-in_ds = tile3
-fn = wd
-data = shdi_arr
-data_type = gdal.GDT_Float32
-nodata = -999
-
-driver = gdal.GetDriverByName('GTiff')
-out_ds = driver.Create(fn, in_ds.RasterXSize, in_ds.RasterYSize, 1, data_type)
-out_ds.SetProjection(in_ds.GetProjection())
-out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-out_band = out_ds.GetRasterBand(1)
-if nodata is not None:
-    out_band.SetNoDataValue(nodata)
-    out_band.WriteArray(data)
-    out_band.FlushCache()
-    out_band.ComputeStatistics(False)
-    #return out_ds
-
-'''
-
-
-
-#####################  ARCHIVE  #####################
-#stacked_data = np.ma.dstack(slices)
-#rows, cols = band1.YSize, band1.XSize #check size raster
-#out_data = np.ones((rows, cols), np.int32) * -99 #raster in original size with only -99 pixel values
-
-# compute SHDI from tif image
-#in_fn = tile1
-#out_fn = 'SHDI_Tile_x26999_y12999_1000x1000.tif'
-#ds = gdal.Open(in_fn)
-# in_band = ds.GetRasterBand(1)
-    #pixel count of relevant categories
-
-
-
-#out_data[5:-5, 5:-5] = #eigene function np.mean(stacked_data, 2)
-'''
-pb.make_raster(in_ds, out_fn, out_data, gdal.GDT_Int32, -99)
-del in_ds
-
-
-
-
-red = ds.GetRasterBand(1).ReadAsArray().astype(np.float)
-nir = ds.GetRasterBand(4).ReadAsArray()
-red = np.ma.masked_where(nir + red == 0, red) #mask the red band
-ndvi = (nir - red) / (nir + red)
-ndvi = ndvi.filled(-99) #fill the empty cells
-
-#out_ds = pb.make_raster(ds, out_fn, ndvi, gdal.GDT_Float32, -99) #set NoData as the fill value
-#overviews = pb.compute_overview_levels(out_ds.GetRasterBand(1))
-#out_ds.BuildOverviews('average', overviews)
-#del ds, out_ds
-'''
-
-
+Array2Raster('3_150.tif', tile3, 0, 0, arr_sl3_150, gdal.GDT_Float64)
+Array2Raster('3_300.tif', tile3, 0, 0, arr_sl3_300, gdal.GDT_Float64)
+Array2Raster('3_450.tif', tile3, 0, 0, arr_sl3_450, gdal.GDT_Float64)
 
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 
