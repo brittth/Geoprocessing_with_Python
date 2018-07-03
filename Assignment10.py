33004771ea75bbe66d3757d19831c757479af3bc
Britta <britta.themann@web.de>
2018-07-03 10:39:08 +0200

    Assignment10 - Notes

--- Assignment10.py
+++ Assignment10.py
@@ -5,6 +5,8 @@ import os
 from osgeo import gdal, ogr, osr
 import numpy as np
 import struct
+from joblib import Parallel, delayed
+import joblib
 
 # ####################################### SET TIME-COUNT ###################################################### #
 
@@ -17,11 +19,15 @@ print("")
 
 # ####################################### FOLDER PATHS & global variables ##################################### #
 
-
+root_folder = "D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 12 - Parallelization/Assignment10_data/"
 
 # ####################################### PROCESSING ########################################################## #
-
-
+#
+#write jobs in list
+#worker function already looks at the list elemests of the job list, so only if the joblist is a list of lists do you have to call job[0],...
+#execute worker function
+if __name__= '__main__':
+    output = Parallel(n_jobs=2) (delayed(SumProd_arrays)(i) for i in array_list)
 
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 
