d89c94d3ec1f78ddb17cbb516e5a120705bbf946
Britta <britta.themann@web.de>
2018-08-09 01:31:22 +0200

    BTheTools

--- BTheTools/raster/__init__.py
+++ BTheTools/raster/__init__.py
@@ -4,8 +4,6 @@ from .RasterOverlapToArray import RasterOverlapToArray
 from .ThresholdBinaryArrayMask import ThresholdBinaryArrayMask
 from .BinArray2Raster import BinArray2Raster
 from .MakeSlices import MakeSlices
-from .CalculateSHDI import CalculateSHDI
-from .CalculateSHDI2Array import CalculateSHDI2Array
 from .InsertArray import InsertArray
 from .Array2Raster import Array2Raster
 from .SpatialReferenceFromRaster import SpatialReferenceFromRaster
